b6234c3735e09f5c742109958294c2aaa5f3c3d0
Ammad Khalid <ammad101khalid@gmail.com>
2018-04-14 07:31:36 +0500

    Updated url_building.py file according to tutorial.

--- url_building.py
+++ url_building.py
@@ -1,3 +1,31 @@
-# TODO: https://www.tutorialspoint.com/flask/flask_url_building.htm
+# https://www.tutorialspoint.com/flask/flask_url_building.htm
 
-from flask import Flask
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+
+@app.route('/')
+def main_page():
+    return "This is main page."
+
+
+@app.route('/admin/')
+def hello_admin():
+    return "Hello Admin!"
+
+
+@app.route('/guest/<guest>/')
+def hello_guest(guest):
+    return "Hello %s as guest." % guest
+
+
+@app.route('/user/<name>')
+def hello_user(name):
+    if name == 'admin':
+        return redirect(url_for('hello_admin'))
+    else:
+        return redirect((url_for('hello_guest', guest=name)))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
