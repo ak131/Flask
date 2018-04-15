73bd81935e89f78fb2f23351c6a21186ca2abe9b
Ammad Khalid <ammad101khalid@gmail.com>
2018-04-15 21:36:10 +0500

    Added student_result files by using form.

---
+++ student_result.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+
+@app.route('/')
+def student():
+    return render_template('student.html')
+
+
+@app.route('/result', methods=['POST'])
+def studnet_result():
+    result = request.form
+    return render_template('student_result.html', result=result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
